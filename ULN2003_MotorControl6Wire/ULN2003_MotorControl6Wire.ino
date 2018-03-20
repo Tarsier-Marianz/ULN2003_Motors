3adb35ab35420abd75ff6892d3b20ba73f058f83
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-03-20 09:02:20 +0800

    * cleaning codes to make it readable

--- ULN2003_MotorControl6Wire/ULN2003_MotorControl6Wire.ino
+++ ULN2003_MotorControl6Wire/ULN2003_MotorControl6Wire.ino
@@ -1,31 +1,26 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 24;  // change this to fit the number of steps per revolution 180degree
-//const int stepsPerRevolution = 48;  // change this to fit the number of steps per revolution 360degree
+const int SPEED                   = 60;  // set the speed at 60 rpm
+const int stepsPerRevolution      = 24;  // change this to fit the number of steps per revolution 180degree
+//const int stepsPerRevolution    = 48;  // change this to fit the number of steps per revolution 360degree
 // for your motor , In my case I used AIRPAX motor which is 7.5degree step angle
 
-//const int SPEED = 60;  // set the speed at 60 rpm:
-const int SPEED = 60;
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper _stepper(stepsPerRevolution, 8, 9, 10, 11); // initialize the stepper library on pins 8 through 11
 
 void setup() {
-  myStepper.setSpeed(SPEED);
-  // initialize the serial port:
-  //Serial.begin(9600);
+  _stepper.setSpeed(SPEED);             // initialize stepper speed
+  Serial.begin(9600);                   // initialize the serial to enable data printing
 }
 
 void loop() {
-  // step one revolution  in one direction:
-  //Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
+  Serial.println("clockwise");
+  _stepper.step(stepsPerRevolution);    // step one revolution  in one direction
   delay(500);
 
-  // step one revolution in the other direction:
   Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
+  _stepper.step(-stepsPerRevolution);   // step one revolution in the other direction
   delay(500);
 
-  //Serial.println(stepsPerRevolution);
+  Serial.println(stepsPerRevolution);
 }
 
